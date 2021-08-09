1177ab29efc25970bbfb00eb29483d40802cd845
Andres <nastybitsz@gmail.com>
2021-08-08 22:15:37 -0400

    arreglar las clases de PDF

---
+++ App2_billmates/classes/flatmate.py
@@ -0,0 +1,8 @@
+class Flatmate:
+    def __init__(self, name, days_in_house):
+        self.name = name
+        self.day_in_house = days_in_house
+    
+    def pays(self, bill, flatmate2):
+        weight = self.day_in_house / (self.day_in_house + flatmate2.day_in_house)
+        return bill.amount * weight
--- App2_billmates/main.py
+++ App2_billmates/main.py
@@ -1,3 +1,9 @@
-class Bill:
-    def __init__(self, amount):
-        self.amount = amount
+
+
+bill = Bill(amount=120, period='21/3/21')
+john = Flatmate(name='John', days_in_house=20)
+marry = Flatmate(name='Marry', days_in_house=25)
+
+pdf_report = PdfReport(filename='Report1.pdf')
+pdf_report.generate(flatmate1=john, flatmate2=marry, bill=bill)
+# print(john.pays(bill=bill, flatmate2=marry))
